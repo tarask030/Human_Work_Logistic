64f367f58eae522ace01424986d32b14639ea04d
tarask030 <38398391+tarask030@users.noreply.github.com>
2021-05-12 14:26:09 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("header").style.height = "90px";
+  } else {
+    document.getElementById("header").style.height = "auto";
+  }
+}
