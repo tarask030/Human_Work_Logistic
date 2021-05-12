8cb372c397054e2f68b60be8317242716708f1c4
tarask030 <38398391+tarask030@users.noreply.github.com>
2021-05-12 23:08:09 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,11 +18,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("header").style.height = "90px";
-    document.getElementById("logo_header").style.marginTop = "20px";
-    document.getElementById("main-menu").style.marginTop = "23px";
+    document.getElementById("header").style.marginTop = "-17px";
   } else {
     document.getElementById("header").style.height = "auto";
-    document.getElementById("logo_header").style.marginTop = "30px";
-    document.getElementById("main-menu").style.marginTop = "30px";
+    document.getElementById("header").style.marginTop = "0px";
   }
 }
